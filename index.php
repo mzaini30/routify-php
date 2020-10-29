3e633dd976530f4608c76b219fe60d06e527dd20
mzaini30 <muhzaini30@gmail.com>
2020-10-29 20:06:34 +0800

    upload

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
+// Autoload files using composer
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Use this namespace
+use Steampixel\Route;
+
+// Add your first route
+Route::add('/', function() {
+  echo 'Welcome :-)';
+});
+
+// Add the first route
+Route::add('/user/([0-9]*)/edit', function($id) {
+  echo 'Edit user with id '.$id.'<br>';
+}, 'get');
+
+// Run the router
+Route::run('/');
+
 function getDirContents($dir, &$results = array()) {
     $files = scandir($dir);
 
