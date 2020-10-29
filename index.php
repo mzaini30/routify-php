557357dd20e290e1713a68eb7d66d9060ed21521
mzaini30 <muhzaini30@gmail.com>
2020-10-29 19:24:41 +0800

    upload

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-halo
+<?php
+function getDirContents($dir, &$results = array()) {
+    $files = scandir($dir);
+
+    foreach ($files as $key => $value) {
+        $path = realpath($dir . DIRECTORY_SEPARATOR . $value);
+        if (!is_dir($path)) {
+            $results[] = $path;
+        } else if ($value != "." && $value != "..") {
+            getDirContents($path, $results);
+            $results[] = $path;
+        }
+    }
+
+    return $results;
+}
+var_dump(getDirContents("pages"));
